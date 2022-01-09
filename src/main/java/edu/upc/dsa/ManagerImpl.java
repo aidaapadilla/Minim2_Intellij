3c8cf006ab4d1d7b5fc5111787639618afda780b
Aida Padilla <aida.padilla.gonzalez.aiko@gmail.com>
2022-01-09 12:39:00 +0100

    versio abans examen

--- src/main/java/edu/upc/dsa/ManagerImpl.java
+++ src/main/java/edu/upc/dsa/ManagerImpl.java
@@ -1,12 +1,59 @@
 package edu.upc.dsa;
 
 import edu.upc.dsa.models.Insignias;
+import edu.upc.dsa.models.User;
 
-public class ManagerImpl {
+import java.util.LinkedList;
+
+public class ManagerImpl implements Manager {
+    private static ManagerImpl manager;
+    public ManagerImpl(){}
+    public static ManagerImpl getInstance(){
+        if(manager==null){
+            manager = new ManagerImpl();
+        }
+        return manager;
+    }
+    LinkedList<Insignias> insignias = new LinkedList<Insignias>();
+    LinkedList<User> users = new LinkedList<User>();
+
     Insignias jugon = new Insignias("El más jugón","http://localhost:8080/public/jugon.png");
     Insignias lento = new Insignias("El más lento","http://localhost:8080/public/lento.png");
     Insignias epico = new Insignias("El más épico","http://localhost:8080/public/epico.png");
     Insignias sostenible = new Insignias("El más sostenible","http://localhost:8080/public/sostenible.png");
     Insignias legendario = new Insignias("El más legendario","http://localhost:8080/public/legendario.png");
     Insignias meteorico = new Insignias("El más meteórico","http://localhost:8080/public/meteorico.png");
+    @Override
+    public LinkedList<Insignias> getInsignias() {
+        insignias.add(jugon);
+        insignias.add(lento);
+        insignias.add(epico);
+        insignias.add(sostenible);
+        insignias.add(legendario);
+        insignias.add(meteorico);
+        return insignias;
+    }
+
+    @Override
+    public User getUser(String user) {
+        LinkedList<Insignias> insigniasUser1 = new LinkedList<Insignias>();
+        insigniasUser1.add(sostenible);
+        insigniasUser1.add(legendario);
+        User user1 = new User("Pepe","papichulo","null",insigniasUser1);
+        users.add(user1);
+
+        LinkedList<Insignias> insigniasUser2 = new LinkedList<Insignias>();
+        insigniasUser2.add(jugon);
+        insigniasUser2.add(lento);
+        User user2 = new User("Paca","lamasguapa","null",insigniasUser2);
+        users.add(user2);
+        int i=0;
+        int res=0;
+        while (i<users.size()){
+            if(users.get(i).getName().equals(user))
+                res=i;
+            i=i+1;
+        }
+        return users.get(res);
+    }
 }
--- src/main/java/edu/upc/dsa/services/Server.java
+++ src/main/java/edu/upc/dsa/services/Server.java
@@ -1,4 +1,62 @@
 package edu.upc.dsa.services;
+import edu.upc.dsa.models.*;
+import edu.upc.dsa.Manager;
+import edu.upc.dsa.ManagerImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.LinkedList;
+import java.util.List;
+
+@Api(value ="/endpoint", description = "Endpoint to Pokemon Service")
+@Path("/endpoint")
 public class Server {
+    private Manager manager;
+
+    public Server(){this.manager =ManagerImpl.getInstance();}
+
+    @GET
+    @ApiOperation(value = "lista de insignias", notes = "asdasd")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful", response = Insignias.class, responseContainer = "Lista"),
+            @ApiResponse(code = 500, message = "Error")
+    })
+    @Path("/insignias")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getInsignias(){
+        LinkedList<Insignias> insignias = this.manager.getInsignias();
+        if(insignias.size()!=0) {
+            GenericEntity<LinkedList<Insignias>> entity = new GenericEntity<LinkedList<Insignias>>(insignias) {
+            };
+            return Response.status(200).entity(entity).build();
+        }
+        else{
+            return Response.status(500).build();
+        }
+    }
+
+    @GET
+    @ApiOperation(value = "get user", notes = "asdasd")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful", response = User.class),
+            @ApiResponse(code = 404, message = "Not found")
+    })
+    @Path("/user/{userId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUser(@PathParam("userId") String userName){
+        if(userName!=null){
+            User user = this.manager.getUser(userName);
+            if (user!=null){
+                return Response.status(201).entity(user).build();}
+            else{
+                return Response.status(500).build();}}
+        else{
+            return Response.status(500).build();}
+    }
 }
